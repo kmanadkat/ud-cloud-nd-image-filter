d7f00e1e069e8d9b1b76983d17ec3e2a5335f235
kmanadkat <kmanadkat@gmail.com>
2022-10-09 09:07:14 +0530

    feat: Added Filter Image Endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,23 +13,38 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  // GET /filteredimage?image_url={{URL}}
+  app.get('/filteredimage', async(req: Request, res: Response) => {
+    try {
+      // 1. validate the image_url query
+      const {image_url} = req.query
+      if(typeof image_url !== "string" || !image_url.startsWith('http')) {
+        // Bad Request Response
+        return res.status(400).json({
+          success: false,
+          message: 'image_url missing or invalid'
+        })
+      }
 
-  /**************************************************************************** */
+      // 2. call filterImageFromURL(image_url) to filter the image
+      const newImagePath = await filterImageFromURL(image_url)
 
-  //! END @TODO1
+      // 3. send the resulting file in the response
+      return res.status(201).sendFile(newImagePath, (err) => {
+        if(err){
+          return res.status(500).json({success: false, message: err.message})
+        }
+        // 4. deletes any files on the server on finish of the response
+        deleteLocalFiles([newImagePath])
+      })
+    } catch (error) {
+      // Server Error
+      console.error(error)
+      return res.status(500).json({success: false, message: 'Internal Server Error'})
+    }
+  })
+
   
   // Root Endpoint
   // Displays a simple message to the user
